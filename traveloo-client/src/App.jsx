b4ff72206558265983d049e9a8480050fba3da76
md-ashikur <ashik76690@gmail.com>
2023-12-03 12:21:14 +0600

    font setup

--- traveloo-client/src/App.jsx
+++ traveloo-client/src/App.jsx
@@ -1,29 +1,35 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+import { Route, Routes, useLocation } from "react-router-dom";
+import SignUp from "./pages/SignUp/SignUp";
+import Home from "./Pages/Home/Home";
+import Discover from "./Pages/Discover/Discover";
+import Offer from "./Pages/Offer/Offer";
+import Contact from "./Pages/Contact/Contact";
+import Login from "./Pages/Login/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
+  }, [location.pathname]);
 
   return (
     <>
-      <div>
-      
-      </div>
-      <h1 className='text-5xl font-bold'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/offer" element={<Offer />} />
+        <Route path="/contact" element={<Contact />} />
+
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- traveloo-client/src/Pages/Home/Home.jsx
+++ traveloo-client/src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            home
+          <h1 className=''>asffgdgr</h1>
         </div>
     );
 };

a680caad07042e2bb73a5ed998bc3ef600f1446e
md-ashikur <ashik76690@gmail.com>
2023-12-04 18:24:19 +0600

    home explore section img update

--- traveloo-client/src/Partials/home/ExploreCards.jsx
+++ traveloo-client/src/Partials/home/ExploreCards.jsx
@@ -1,36 +1,51 @@
-import pic1 from "../../assets/images/New folder/explore1.jpg";
-import pic2 from "../../assets/images/New folder/explore1.jpg";
-import pic3 from "../../assets/images/New folder/explore1.jpg";
-import pic4 from "../../assets/images/New folder/green.jpg";
-
-
 
 const ExploreCards= [
     
         {
           "id": "1",
-          "picture": {pic1},
+          "picture": "https://images.pexels.com/photos/1433052/pexels-photo-1433052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
           "title": "Paradise Beach, Bantayan Island",
           "price": "$550.16",
           "description": "Rome, Italy"
         },
         {
           "id": "2",
-          "picture": {pic2},
+          "picture": "https://images.pexels.com/photos/931018/pexels-photo-931018.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
           "title": "Ocean with full of Colors",
           "price": "$40.50",
           "description": "Maldives"
         },
         {
           "id": "3",
-          "picture": {pic3},
+          "picture": "https://images.pexels.com/photos/1232594/pexels-photo-1232594.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
           "title": "Mountain View, Above the cloud",
           "price": "$200.10",
           "description": "United Arab Emeries"
         },
         {
           "id": "4",
-          "picture": {pic4},
+          "picture": "https://images.pexels.com/photos/788200/pexels-photo-788200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          "title": "Forest with full of green",
+          "price": "$175.79",
+          "description": "Bangladesh"
+        },
+        {
+          "id": "5",
+          "picture":"https://images.pexels.com/photos/1743165/pexels-photo-1743165.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          "title": "Mountain View, Above the cloud",
+          "price": "$200.10",
+          "description": "United Arab Emeries"
+        },
+        {
+          "id": "6",
+          "picture": "https://images.pexels.com/photos/775201/pexels-photo-775201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          "title": "Forest with full of green",
+          "price": "$175.79",
+          "description": "Bangladesh"
+        },
+        {
+          "id": "7",
+          "picture": "https://images.pexels.com/photos/1173777/pexels-photo-1173777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
           "title": "Forest with full of green",
           "price": "$175.79",
           "description": "Bangladesh"
--- traveloo-client/src/Partials/home/HExplore.jsx
+++ traveloo-client/src/Partials/home/HExplore.jsx
@@ -2,70 +2,74 @@ import React, { useState } from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import ExploreCards from "./ExploreCards";
-import pic4 from "../../assets/images/New folder/forest.jpg";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
-import 'swiper/css/navigation';
 
 // import required modules
-import { Navigation } from 'swiper/modules';
+import {  Navigation } from "swiper/modules";
 
 const HExplore = () => {
-  const [slides, setSlides] = useState(ExploreCards);
+  const [slides] = useState(ExploreCards);
 
   const navigationPrevRef = React.useRef(null);
   const navigationNextRef = React.useRef(null);
 
   return (
-    <section className="px-28 my-20">
-      <div className="flex justify-between items-center">
+    <section className="px-28 my-20 max-w-[1300px] overflow-hidden">
+      <div className="flex justify-between items-center ">
         <div>
           <h4 className="text-primary">Top Destination</h4>
           <h2>Explore top destination</h2>
         </div>
+
         <div className="flex gap-5">
-          <button ref={navigationPrevRef} className=" hover:bg-secondary hover:text-base border hover:border-secondary transition ease-in-out delay-150  hover:scale-110 hover:shadow-lg hover:shadow-indigo-500/50 rounded-full flex items-center justify-center  w-16 h-16 p-5">
+          <button
+            ref={navigationPrevRef}
+            className=" hover:bg-secondary hover:text-base border hover:border-secondary transition ease-in-out delay-150  hover:scale-110 hover:shadow-lg hover:shadow-indigo-500/50 rounded-full flex items-center justify-center  w-16 h-16 p-5"
+          >
             <FaArrowLeftLong />
           </button>
-          <button ref={navigationNextRef} className="hover:bg-secondary hover:text-base border hover:border-secondary transition ease-in-out delay-150  hover:scale-110 hover:shadow-lg hover:shadow-indigo-500/50 rounded-full flex items-center justify-center  w-16 h-16 p-5">
+          <button
+            ref={navigationNextRef}
+            className="hover:bg-secondary hover:text-base border hover:border-secondary transition ease-in-out delay-150  hover:scale-110 hover:shadow-lg hover:shadow-indigo-500/50 rounded-full flex items-center justify-center  w-16 h-16 p-5"
+          >
             <FaArrowRightLong />
           </button>
         </div>
       </div>
 
       {/* cards----------- */}
-      <div className=" relative ">
+      <div className="  ">
         <Swiper
-          navigation={{
-            prevEl: navigationPrevRef.current,
-            nextEl: navigationNextRef.current,
-          }}
+         
           onBeforeInit={(swiper) => {
             swiper.params.navigation.prevEl = navigationPrevRef.current;
             swiper.params.navigation.nextEl = navigationNextRef.current;
           }}
           breakpoints={{
+            360: {
+              slidesPerView: 1,
+              spaceBetween: 50,
+            },
             390: {
               slidesPerView: 1,
-              spaceBetween: 20,
+              spaceBetween: 10,
             },
             768: {
-              slidesPerView: 2,
+              slidesPerView: 3,
               spaceBetween: 40,
             },
             1024: {
-              slidesPerView: 3,
+              slidesPerView: 4,
               spaceBetween: 50,
             },
+           
           }}
           loop={true}
-          loopFillGroupWithBlank={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
+          loopfillgroupwithblank="true"
+          spaceBetween={30}
           modules={[ Navigation]}
           className="mySwiper my-5 py-5 "
         >
@@ -73,15 +77,16 @@ const HExplore = () => {
             const { id, picture, title, price, description } = elem;
 
             return (
-              <SwiperSlide key={id} className="shadow-lg w-[300px] h-[470px] rounded-[32px] overflow-hidden">
+              <SwiperSlide
+                key={id}
+                className="explorer-slide shadow-lg  rounded-[32px] overflow-hidden"
+              >
                 <div className="w-full h-[300px] overflow-hidden">
-                  <img src={pic4} alt="" className="h-auto w-full"/>
+                  <img src={picture} alt="" className="h-auto w-auto" />
                 </div>
                 <div className="p-5">
                   <div className="grid grid-cols-3">
-                    <h5 className="col-span-2">
-                     {title}
-                    </h5>
+                    <h5 className="col-span-2">{title}</h5>
                     <h5 className="text-primary text-right">{price}</h5>
                   </div>
                   <p className="mt-3">{description}</p>
